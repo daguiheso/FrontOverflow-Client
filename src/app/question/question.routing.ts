a1e149177bbfd1d25fee86f41f0e7a20c1268e85
daguiheso <daguiheso@gmail.com>
2018-03-10 21:47:28 -0500

    Connect questions list with form question create

--- src/app/question/question.routing.ts
+++ src/app/question/question.routing.ts
@@ -1,9 +1,10 @@
 import { Routes, RouterModule } from '@angular/router';
 import { QuestionListComponent } from './question-list/question-list/question-list.component';
 import { QuestionDetailComponent } from './question-detail/question-detail.component';
-
+import { QuestionFormComponent } from './question-form/question-form.component';
 
 export const QUESTION_ROUTES: Routes = [
 	{ path: '', component: QuestionListComponent },
+	{ path: 'new', component: QuestionFormComponent },
 	{ path: ':id', component: QuestionDetailComponent }
 ];
