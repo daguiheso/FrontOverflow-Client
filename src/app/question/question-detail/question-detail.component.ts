a34b4492daff9dca276181d1f4813c52ad6dae8c
Daniel Hernandez <daguiheso@gmail.com>
2018-03-28 12:15:05 -0500

    Get detail question from server

--- src/app/question/question-detail/question-detail.component.ts
+++ src/app/question/question-detail/question-detail.component.ts
@@ -1,24 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Question } from '../question.model';
+import { QuestionService } from '../services/question.service';
 
 @Component({
   selector: 'app-question-detail',
   templateUrl: './question-detail.component.html',
-  styleUrls: ['./question-detail.component.css']
+  styleUrls: ['./question-detail.component.css'],
+  providers: [QuestionService]
 })
 
-export class QuestionDetailComponent implements OnInit {
+export class QuestionDetailComponent implements OnInit, OnDestroy {
 
-  question: Question = new Question(
-    'Esta es una nueva pregunta sobre Typescript',
-    'Miren, tengo una duda con una web platform y quiero hacerla con typescript, se puede?',
-    new Date,
-    'devicon-typescript-plain colored'
-  );
+  question?: Question;
+  loading = true;
+  sub: any;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private questionService: QuestionService
+  ) { }
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      this.questionService
+        .getQuestion(params.id)
+        .then((question: Question) => {
+          this.question = question;
+          this.loading = false;
+        });
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
